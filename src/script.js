8c70601778a4da93f92b9d5119338cd438113fa0
vishal-pundhir <vishal.pundhir@spyne.ai>
2025-05-19 07:36:50 +0530

    created branches

--- src/script.js
+++ src/script.js
@@ -15,19 +15,75 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+//Galaxy
+
+const parameters = {}
+parameters.count = 100000;
+parameters.size = 0.01;
+parameters.radius = 5;
+parameters.branches = 3;
+parameters.spin = 3;
+
+let geometry = null;
+let material = null;
+let points = null;
+
+
+const generateGalaxy = () => {
+
+    //Destroy if points already exists (destroy old galaxy)
+    if (points) {  
+        geometry.dispose();
+        material.dispose();
+        scene.remove(points);
+    }
+
+    //Geometry
+    geometry = new THREE.BufferGeometry();
+
+    const positions = new Float32Array(parameters.count * 3);
+
+    for(let i=0; i < parameters.count; i++){
+        const i3 = i * 3;
+        
+        const radius = Math.random() * parameters.radius;
+        const spinAngle = radius * parameters.spin;
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+
+        positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius;
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius;
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+    //Material
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending
+    })
+
+    //Points
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}
+generateGalaxy()
+
+//Tweaks
+gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(10).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'spin').min(-5).max(20).step(1).onFinishChange(generateGalaxy)
+
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
 
 /**
  * Sizes
